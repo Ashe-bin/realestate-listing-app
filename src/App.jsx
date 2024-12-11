8ec73b45fec2fdec535db4cb8729d7f539299256
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-12-11 16:28:39 +0300

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Profile } from "./pages/Profile";
+import { About } from "./pages/About";
+
 function App() {
   return (
     <>
-      <div className="text-red-600 "> first run</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
