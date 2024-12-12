dc2443d8dd4218bd7f54055ee60267bd03146685
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-12-12 12:13:40 +0300

    create signup route

--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
@@ -11,8 +12,11 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
--- Api/models/user.model.js
+++ Api/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose(
+const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
@@ -8,5 +8,5 @@ const userSchema = new mongoose(
   },
   { timestamps: true }
 );
-const user = mongoose.model("User", userSchema);
-export default user;
+const User = mongoose.model("User", userSchema);
+export default User;
