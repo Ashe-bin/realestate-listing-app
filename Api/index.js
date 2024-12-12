ebdc0ad3b26c3361bf760e56e2092c45bbe89501
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-12-12 10:00:06 +0300

    connected to mongoose

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGOOSE_CONNECT)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 const app = express();
 
 app.listen(3000, () => {
