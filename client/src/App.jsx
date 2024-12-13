e575746dde800befb3c65197caec2234e90241e0
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-12-13 15:33:43 +0300

    update the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Profile } from "./pages/Profile";
 import { About } from "./pages/About";
 import { Header } from "./components/Header";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -15,7 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/About" element={<About />} />
         </Routes>
       </BrowserRouter>
