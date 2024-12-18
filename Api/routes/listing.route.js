297e5f3b8b70e02e1c75def1787eda27ae8d08a2
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-12-18 12:09:44 +0300

    create search api route

--- Api/routes/listing.route.js
+++ Api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   editListing,
   getListing,
+  getListings,
 } from "../controller/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -12,4 +13,5 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/edit/:id", verifyToken, editListing);
 router.get("/getListing/:id", getListing);
+router.get("/getListing", getListings);
 export default router;
